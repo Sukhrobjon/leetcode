49c8daca62126547abd7a506bc7400a6a45fa798
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-06-23 18:50:10 -0700

    [completed] breakout problem

---
+++ SPD-1.01/find_middle_node.py
@@ -0,0 +1,28 @@
+"""
+    Question: Find the middle item in a singly linked list, 
+    or two middle items if it contains an even number of nodes.
+"""
+
+def find_middle_node(ll):
+    """
+        it is assumed the linked list class is implemented as in class
+    """
+
+    if ll.size % 2 == 0:
+        curr = head
+        i = 0
+        while curr:
+            curr = curr.next
+            i += 1
+            if i == (ll.size // 2) + 1
+                return (curr.data, curr.next.data)
+    else:
+        curr = head
+        i = 0
+        while curr:
+            curr = curr.next
+            i += 1
+            if i == (ll.size // 2) + 1
+                return curr.data
+
+
