1ed6f6b0bb68ae20b278415547b67debd5917ad9
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-09-02 21:27:15 -0700

    [solved] remove duplicate from sorted array

---
+++ remove_duplicates_sorted_array.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def remove_duplicates(self, nums):
+        """Removes duplicate number from sorted array in place
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in range(len(nums)-1, 0, -1):
+            if nums[i] == nums[i-1]:
+                # print(f"num[i] {nums[i]}, [i+1] {nums[i+1]}")
+                del nums[i-1]
+
+        return nums
+
+
+nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+
+obj = Solution()
+print(obj.remove_duplicates(nums))
