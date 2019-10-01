812179e681c10bfd6193be3c486bbddd43e8fa89
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-09-30 22:25:30 -0700

    [implemented] reverse string

---
+++ reverse_string.py
@@ -0,0 +1,29 @@
+"""
+Write a function that reverses a string. The input string is given as an array
+of characters char[].
+
+Do not allocate extra space for another array, you must do this by modifying
+the input array in-place with O(1) extra memory.
+
+You may assume all the characters consist of printable ascii characters.
+"""
+
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        front = 0
+        back = len(s) - 1
+        while front < back:
+            s[front], s[back] = s[back], s[front]
+            front += 1
+            back -= 1
+        return s
+
+obj = Solution()
+word = ["H", "a", "n", "n", "a", "h"]
+result = obj.reverseString(word)
+print(result)
