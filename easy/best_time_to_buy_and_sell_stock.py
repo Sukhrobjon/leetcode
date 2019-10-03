8dd8ce3f33f29c985e62bec0732013e0d77d5c55
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-10-02 22:47:48 -0700

    [solving] best time to buy and sell II

--- easy/best_time_to_buy_and_sell_stock.py
+++ easy/best_time_to_buy_and_sell_stock.py
@@ -4,29 +4,10 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        # find the min number
-        # and find the max number after min num's index
-        # if there is no one return 0 else return 
-        # max - min
-        # if not prices:
-        #     return 0
-        # min = [prices[0], 0]
-        # for i, price in enumerate(prices):
-        #     if price < min[0]:
-        #         min = [price, i]
-        # buy, location = min[0], min[1]
-
-        # # if buy is last day then we cant sell it anymore
-        # if buy == prices[-1]:
-        #     return 0
-        # # find the max price from rest of the min price
-        # sell = max(prices[location+1:])
-
-        # profit = sell - buy
-        # return profit if profit > 0 else 0
         if not prices:
             return 0
         profit = 0
+        total = 0
         min_price = prices[0]
         for price in prices:
             # find the min so far
@@ -37,12 +18,16 @@ class Solution(object):
             current_profit = price - min_price
             # find which profit is bigger current profit or the profit we
             # saw so far
-            profit = max(profit, current_profit)
-
+            if current_profit > profit:
+                profit = current_profit
+                total += profit
+                print("total: ", total)
+            # profit = max(profit, current_profit)
+        # print(total)
         return profit
 
 
-prices = []
+prices = [7, 1, 5, 3, 6, 4]
 obj = Solution()
 result = obj.max_profit(prices)
 print(result)
