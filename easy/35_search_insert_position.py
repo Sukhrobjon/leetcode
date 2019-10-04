f62f490db41669309ac345b0de9024ac51b2e1ec
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-10-03 21:06:20 -0700

    [added] other solution from leetcode

--- easy/35_search_insert_position.py
+++ easy/35_search_insert_position.py
@@ -1,6 +1,7 @@
 class Solution(object):
     def search_insert(self, nums, target):
         """
+        Naive solution 40 ms
         :type nums: List[int]
         :type target: int
         :rtype: int
@@ -17,13 +18,13 @@ class Solution(object):
 
     def search_insertion_v2(self, nums, target):
         """
-        Binary search
+        my solution 36ms:
         """
-        if not nums:
+        if not nums or nums[0] == target:
             return 0
+
         left = 0
         right = len(nums)-1
-        count = 0
 
         # at the beginning
         if target < nums[left]:
@@ -35,29 +36,60 @@ class Solution(object):
         # then we know the target should be inserted somewhere in the middle
         while left < right:
             middle = (left + right) // 2
-            count += 1
-            print(f"middle: {nums[middle]}, i: {middle}")
+
             if target == nums[middle]:
-                print("count: ", count)
+
                 return middle
 
             if target > nums[middle]:
                 left = middle + 1
                 if target <= nums[left]:
-                    print("found left")
                     return left
-                print("left: ", left)
-            elif target < nums[middle]:
-            # elif:
+
+            # elif target < nums[middle]:
+            else:
                 right = middle - 1
-                print("right: ", right)
-                if target >= nums[right]:
-                    print("found right")
+
+                if target == nums[right]:
                     return right
-        # print(f"middle: {middle}")
-        return "Not found"
-nums = [1, 3, 5, 6]
-target = 2
+                elif target > nums[right]:
+                    return right + 1
+
+    def search_insertion_v3(self, nums, target):
+        """
+        Binary search: 24ms solution from leetcode
+        """
+        if not nums:
+            return 0
+        left = 0
+        right = len(nums) - 1
+
+
+        # at the beginning
+        if target < nums[left]:
+            return left
+        # or at the end of the list
+        if target > nums[right]:
+            return right + 1
+
+        # then we know the target should be inserted somewhere in the middle
+        while left <= right:
+            middle = (left + right) // 2
+            if target == nums[middle]:
+                return middle
+
+            if target > nums[middle] and nums[middle + 1] > target:
+                return middle + 1
+            else:
+                if target < nums[middle]:
+                    right = middle
+                else:
+                    left = middle + 1
+
+
+
+nums = [3, 6, 7, 8, 10]
+target = 5
 obj = Solution()
-result = obj.search_insertion_v2(nums, target)
+result = obj.search_insertion_v3(nums, target)
 print(f"result: {result}, target: {target}")
