780a3c2dae1e956fc19f69cc4a5055d8d14c6b8a
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2020-01-27 22:22:52 -0800

    [debugging] longest common prefix

--- easy/14_longest_common_prefix.py
+++ easy/14_longest_common_prefix.py
@@ -7,14 +7,27 @@ class Solution(object):
         # compare the first element of all string in the list
         # until find the mismatch
         # first letter of first word in the list
+        # initial prefix is whole word of the 0th element of the list
         prefix = strs[0]
-        common_index = 0
-        for i in range(1, len(strs)-1):
-            
-
-        return common_index
 
 
-strs = ["flower", 'ffff', "flow", "flight"]
+        for i in range(1, len(strs)):
+            word = strs[i]
+            bound = min(len(prefix), len(word))
+            for j in range(0, bound):
+                print(word)
+                # print(f"prefix[j]: {prefix[j]}, word[j]: {word[j]}")
+                if prefix[j] != word[j] and j == 0:
+                    # print(f"prefix[j]: {prefix[j]}, word[j]: {word[j]}")
+                    return ""
+                elif prefix[j] != word[j]:
+                    break
+                
+            prefix = prefix if len(prefix) <= len(word[0:j]) else word[0:j]
+            print(f"pre: {prefix}")
+        return prefix
+
+
+strs = ["flower", "flow", "flight", 'f']
 obj = Solution()
 print("prefix: ", obj.longestCommonPrefix(strs))
