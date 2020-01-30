19a817ee6beaf2b3f2c8ccb284965fd5dc9990f3
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2020-01-30 15:31:41 -0800

    [finished] longest common prefix problem

--- easy/14_longest_common_prefix.py
+++ easy/14_longest_common_prefix.py
@@ -4,42 +4,39 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        # if not strs:
-        #     return ""
-        # # first letter of first word in the list
-        # # initial prefix is whole word of the 0th element of the list
-        # prefix = strs[0]
+        if not strs:
+            return ""
+        prefix = strs[0]
+        for i in range(1, len(strs)):
+            word = strs[i]
+            print("word: ", word)
+            prefix = self.common_prefix(prefix, word)
+            print("each word", prefix)
+            # if we have empty prefix no need to compare the rest
+            if prefix == "":
+                return ""
+        
+        return prefix
 
-        # # iterate through list of words
-        # for i in range(1, len(strs)):
-            
-        #     word = strs[i]
-        #     if len(word) == 0:
-        #         return ""
-        #     # compare the jth element of each word against the prefix[j]
-        #     for j in range(len(word)):
-        #         print(word)
-        #         # print(f"prefix[j]: {prefix[j]}, word[j]: {word[j]}")
-        #         # no common prefix when the 0th element doesnt match
-        #         if prefix[j] != word[j] and j == 0:
-        #             # print(f"prefix[j]: {prefix[j]}, word[j]: {word[j]}")
-        #             return ""
-        #         # unmatch
-        #         elif prefix[j] != word[j]:
-        #             break
-        #     # update the prefix, choose min of length
-        #     prefix = prefix if len(prefix) < len(word[0:j]) else word[0:j]
-        #     print(f"pre: {prefix}")
-        # return prefix
-        def longest_common_prefix(self, strs):
-            """
-            
-            """
-
-# strs = ["flower", "flow", "flight"]
-strs = ["b", ""]
+    def common_prefix(self, word_1, word_2):
+        """
+        Returns the common prefix of two words
+        """
+        bound = min(len(word_1), len(word_2))
+        # if one of the strings are empty string
+        if bound == 0:
+            return ""
+        for i in range(bound):
+            if word_1[i] != word_2[i] and i == 0:
+                return ""
+            elif word_1[i] != word_2[i]:
+                return word_1[0:i]
+        return word_1[0:bound]
+# strs = ["flower", "flow", "flight", "d"]
+strs = ["x", "x"]
 # ["c","c"], ["", "b"]
 obj = Solution()
-print("prefix: ", obj.longestCommonPrefix(strs))
+print("prefix: ", obj.longestCommonPrefix([]))
+print("two word: ", obj.common_prefix(strs[0], strs[1]))
 
 
