68dd74c9d1e26f696256185d4bfc6aa7565f62aa
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2020-02-05 22:47:55 -0800

    [cleaned] up in order traversal

--- medium/94_binary_tree_inorder_traversal.py
+++ medium/94_binary_tree_inorder_traversal.py
@@ -5,8 +5,8 @@
 #         self.left = None
 #         self.right = None
 
-
 class Solution(object):
+
     def inorderTraversal(self, root):
         """
         :type root: TreeNode
@@ -23,52 +23,14 @@ class Solution(object):
         Helper funtion to traverse the tree inorder
         """
         stack = []
-
-        # add the root node into the stack
-        stack.append(node)
-
-        while stack:
-            curr_node = stack[-1]  # top of the stack
-            # check if node has left child
-            if curr_node.left:
-                # we add that node into the stack
-                stack.append(curr_node.left)
-            else:  # the node is the farthest left node
-                # pop it
+        while stack or node:
+            if node is not None:
+                # push it to stack
+                stack.append(node)
+                node = node.left
+            else:
+                # pop top of the stack
                 node = stack.pop()
-                # visit the node data
                 visit(node.val)
-                # check if we need to pop the parent of that node
-                if len(stack) != 0:
-                    node = stack[-1]
-                    visit(node.val)
-                if node.right:
-                    # push it into stack
-                    stack.append(node.right)
-
-
-class Solution(object):
-
-    def inorderTraversal(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[int]
-        """
-        items = []
-        if root is not None:
-            self._iterative_inorder(root, items.append)
-
-        return items
-
-    def _iterative_inorder(self, node, visit):
-        """
-        Helper funtion to traverse the tree inorder
-        """
-        stack = []
-        stack.append(node)  # push the root node
-        while stack:
-            while stack[-1].left is not None:
-                stack.append(stack[-1].left)
-            node = stack.pop()
-            visit(node.val)
-            node = node.right
+                # move node to the right
+                node = node.right
