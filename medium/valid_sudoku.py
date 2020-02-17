2fca28efcc624c4ceea9f004724b4681615db535
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2020-02-17 10:52:18 -0800

    [added] valid sudoku

---
+++ medium/valid_sudoku.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+    
+    def check_row(self):
+        pass
+    
+    def check_column(self):
+        pass
+
+    def check_3_x_3_box(self):
+        pass
+
+if __name__ == "__main__":
+    pass
