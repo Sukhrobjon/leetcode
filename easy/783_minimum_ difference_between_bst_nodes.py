d910a08861cc641a5875d2ec27ef713ed300a2e1
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2020-02-24 21:34:10 -0800

    [added] minimum difference between nodes in bst

--- easy/783_minimum_ difference_between_bst_nodes.py
+++ easy/783_minimum_ difference_between_bst_nodes.py
@@ -12,11 +12,37 @@ difference between the values of any two different nodes in the tree.
 difference between pairs, which takes O(n^2)
 2. For each node find the closest number to it, which takes O(nlogn), because we search
 for closer number in logn time for each n nodes.
-3. 
+3. Do in order traversal and Compare the adjacents, keep track of min difference 
 """
+
+
 class Solution(object):
+    # smallest number possible
+    prev = -float('inf')
+    # the biggest possible difference
+    difference = float('inf')
+
     def minDiffInBST(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
+
+        self.in_order(root)
+        return self.difference
+
+    def in_order(self, node, prev=None, difference=None):
+        if node is None:
+            return
+
+        # if prev is None and difference is None:
+        #     prev = -float('inf')
+        #     difference = float('inf')
+
+        self.in_order(node.left)
+
+        self.difference = min(node.val-self.prev, self.difference)
+        # update the prev value to current node's val
+        self.prev = node.val
+        # move to the right side of node
+        self.in_order(node.right)
