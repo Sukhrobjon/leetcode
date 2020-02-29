2769663ed7d006d811a8e814f34edf4c1456cfff
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2020-02-28 21:37:26 -0800

    [added] path sum

---
+++ easy/112_path_sum.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+
+        path = self.helper(root, sum)
+        print(path)
+        return True if path else False
+
+    def helper(self, node, sum):
+        # if path is None:
+        #     path = []
+        # when we find the path the node is leaf
+
+        if node is None:
+            return False
+        # we get to the leaf
+        if node.left is None and node.right is None:
+            # check if it is right path
+            if sum == node.val:
+                return True
+
+        if 
