1a6b7fa876a5171a4c5abd80a7c1d265b8961c17
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2020-03-14 17:40:24 -0700

    [added] finding middle node of the linked likst

---
+++ easy/876_middle_of_linked_list.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        slow = head
+        fast = head
+
+        while fast is not None and fast.next is not None:
+
+            # move fast and slow pointer accordingly
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
