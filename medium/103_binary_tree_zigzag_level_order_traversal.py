08afadd2ae9473965159bcfc4bc2701baf73df97
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2020-03-20 23:09:45 -0700

    [changed] zigzag level order

--- medium/103_binary_tree_zigzag_level_order_traversal.py
+++ medium/103_binary_tree_zigzag_level_order_traversal.py
@@ -20,11 +20,11 @@ return its zigzag level order traversal as:
 
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 
 class Solution:
@@ -34,3 +34,37 @@ class Solution:
             Space complexity: O()
         """
         # queue for regular level order
+        queue = []
+        # keep the zigzag order for coressponding levels(odd levels)
+        stack = []
+        level = 0
+        zigzag_order = []
+        queue.append((root, level))
+
+        while queue:
+            # pop the front of the queue
+            node, level = queue.pop(0)
+
+            # add its children and child's level
+            if node.left:
+                queue.append((node.left, level+1))
+
+            if node.right:
+                queue.append((node.left, level+1))
+
+            # if level of current node is odd that means we need to add it to
+            # result in reversed order
+            front_level = queue[0][1]
+            # if the next node's level is even now we need to empty out the stack and put it
+            # into output result
+            if front_level % 2 == 0:
+                zigzag_order[level] = []
+                while len(stack) != 0:
+                    # pop the top of the stack
+                    node = stack.pop()
+                    zigzag_order[level].append(node.val)
+            if level % 2 != 0:
+                # we put them in stack
+                stack.append(node)
+            # otherwise we perform the regular order
+            else:
