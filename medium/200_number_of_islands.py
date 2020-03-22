37a0d9ae8f90898c3ebc401ff9642e4e31c345b6
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2020-03-21 19:27:01 -0700

    [implemented] number of islands problem

--- medium/200_number_of_islands.py
+++ medium/200_number_of_islands.py
@@ -4,6 +4,64 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-
-
         # use stack and depth first search
+        island = 0
+        for row in range(len(grid)):
+            for column in range(len(grid[row])):
+                if grid[row][column] == '1':
+                    island += 1
+                    self.expand_island(grid, row, column)
+
+        return island
+
+    def expand_island(self, grid, row, column):
+        """
+            Helper function to perform DFS search on the grid
+        """
+
+        # bound check
+        if row < 0 or row >= len(grid) or column < 0 or column >= len(grid[row]):
+            return
+        if grid[row][column] == '0':
+            return
+        
+        grid[row][column] = '0'
+
+        # otherwise we will cal the function recursively
+        self.expand_island(grid, row-1, column) # up
+        self.expand_island(grid, row+1, column)  # down
+        self.expand_island(grid, row, column+1)  # right
+        self.expand_island(grid, row, column-1)  # left
+    
+    def area_of_island(self, grid, row, column):
+        """
+            Find the area of an island in the matrix
+        """
+        if row < 0 or row >= len(grid) or column < 0 or column >= len(grid[row]):
+            return
+        if grid[row][column] == '0':
+            return
+        
+        grid[row][column] = '0'
+        
+
+
+obj = Solution()
+grid_1 = [
+        ["1", "1", "1", "1", "0"],
+        ["1", "1", "0", "1", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "0", "0", "0"]
+        ]
+
+grid_2 = [
+            ["1", "1", "0", "0", "0"],
+            ["1", "1", "0", "0", "0"],
+            ["0", "0", "1", "0", "0"],
+            ["0", "0", "0", "1", "1"]
+        ]
+
+grid = grid_2
+result = obj.numIslands(grid)
+print(grid)
+print(result)
