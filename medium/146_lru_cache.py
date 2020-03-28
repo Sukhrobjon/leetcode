fa885e3e549fff0dc00876ba6cfaf4d342b07d36
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2020-03-28 13:23:23 -0700

    [implemented] lru cache design question

--- medium/146_lru_cache.py
+++ medium/146_lru_cache.py
@@ -66,6 +66,30 @@ class LRUCache(object):
         self.head = None
         self.tail = None
 
+    def __str__(self):
+        """Return a formatted string representation of this linked list"""
+        items = ['({!r})'.format(item) for item in self.items()]
+        return '[{}]'.format(' -> '.join(items))
+
+    def items(self):
+        """
+            Return a list of all items in this linked list.
+            Best and worst case running time: Theta(n) for n items in the list
+            because we always need to loop through all n nodes.
+        """
+        # Create an empty list of results
+        result = []  # Constant time to create a new list
+        # Start at the head node
+        node = self.head  # Constant time to assign a variable reference
+        # Loop until the node is None, which is one node too far past the tail
+        while node is not None:  # Always n iterations because no early exit
+            # Append this node's data to the results list
+            result.append(node.key)  # Constant time to append to a list
+            # Skip to the next node
+            node = node.next  # Constant time to reassign a variable
+        # Now result contains the data from all nodes
+        return result  # Constant time to return a list
+
     def get(self, key):
         """
         Takes key and returns its value if it exists, otherwise -1
@@ -74,7 +98,10 @@ class LRUCache(object):
         """
 
         if self.contains(key):
-            val = self.cache[key].value
+            val = self.move_to_head(key)
+            # we need to put it at the head now because each time,
+            # we interact with that node it has to move to head
+            # call move_to_head()
             return val
 
         # if key not in there
@@ -86,7 +113,102 @@ class LRUCache(object):
         :type value: int
         :rtype: None
         """
-        pass
+        print(f"putting {key}, {value}")
+        # if the key is exist and the value is new we simple update its value
+        # and move it to the head
+        if key in self.cache:
+            print("Existing key being updated!")
+            # get the node
+            node = self.cache[key]
+            # update its value
+            node.value = value
+            # move it to the head
+            self.move_to_head(key)
+        # else, key is new so we need create and insert it at the head
+        else:
+            # create a new node
+            node = DLLNode(key, value)
+            # have a reference to node
+            # if the cache is empty
+            if self.head is None:
+                # head and tail points to node
+                print("Putting item in empty list!")
+                self.cache[key] = node
+                self.head = node
+                self.tail = node
+                
+            # if there is enough space in cache
+            elif self.length() < self.capacity:
+                self.cache[key] = node
+                print("there is enough space")
+                self.update_head(key)
+            # there is no space so we need to remove the tail and insert the new node at head
+            else:
+                print("Putting item into list and remove tail!")
+                # first insert the item at the head
+                self.cache[key] = node
+                self.update_head(key)
+                # remove the tail
+                self.remove_tail()
+
+    def move_to_head(self, key):
+        """
+            Move existing node in the list to head
+        """
+
+        # access the node
+        node = self.cache[key]
+        # if node is head
+        if self.head == node:
+            # do nothing
+            return node.value
+
+        # if the node is tail
+        if self.tail == node:
+            node.prev.next = node.next
+            # make the node. prev as new tail
+            self.tail = node.prev
+            # now move the node to the head
+            self.update_head(key)
+            return node.value
+
+        # ============if node is in the middle not head or tail================
+        # link two adjecent nodes together
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        # now move the node to the head
+        self.update_head(key)
+
+        return node.value
+
+    def update_head(self, key):
+        """
+            move update the head with the node with given key
+        """
+        # get the node
+        node = self.cache[key]
+
+        node.next = self.head
+        self.head.prev = node
+        node.prev = None
+        self.head = node
+    
+    def remove_tail(self):
+        """
+            remove the current tail and update it
+        """
+        node = self.tail
+        node.prev.next = None
+        self.tail = node.prev
+        key = node.key
+        # delete the node from the dict cache
+        del self.cache[key]
+  
+    def length(self):
+        """
+            returns the length of the cache
+        """
+        return len(self.cache)
 
     def contains(self, key):
         """
@@ -96,9 +218,23 @@ class LRUCache(object):
             return True
         return False
         
+if __name__ == "__main__":
+    capacity = 2
+    cache = LRUCache(capacity)
 
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+    cache.put(1, 1)
+    cache.put(2, 2)
+    print(cache.__str__())
+    print(cache.get(1))
+    print(cache.__str__())
+    cache.put(3, 3)  # evicts 2
+    print(cache.__str__())
+    print(cache.get(2))  # -1
+    cache.put(4, 4)  # evicts 
+    print(cache.__str__())
+    print(cache.get(1))  # -1
+    print(cache.__str__())
+    print(cache.get(3))
+    print(cache.__str__())
+    print(cache.get(4))
+    print(cache.__str__())
