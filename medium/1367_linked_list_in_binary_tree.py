484c2dd3f6f8dc3330c11d0663c818830f8a7b88
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2020-03-31 20:29:17 -0700

    [implemented] linked list in binary tree

--- medium/1367_linked_list_in_binary_tree.py
+++ medium/1367_linked_list_in_binary_tree.py
@@ -19,3 +19,32 @@ class Solution(object):
         :type root: TreeNode
         :rtype: bool
         """
+
+        if head is None:
+            return True
+        
+        if root is None:
+            return False
+
+        # i wrote them more readable lines but if we dont use return values and
+        # instead call them directly runs much faster, calling them directly
+        # runs 112-116ms, and calling with return values runs 228ms
+        # NOTE: next 4 lines are more readbable version of the return statement
+        # start_from_root = self.find_path(head, root)
+        # start_from_left = self.isSubPath(head, root.left)
+        # start_from_right = self.isSubPath(head, root.right)
+        # return start_from_root or start_from_left or start_from_right
+        return self.find_path(head, root) or self.isSubPath(head, root.left) or self.isSubPath(head, root.right)
+
+    def find_path(self, head, root):
+        """
+            find a path from given root and head of the linked list
+        """
+
+        if head is None:
+            return True
+        if root is None:
+            return False
+        
+        if head.val == root.val:
+            return self.find_path(head.next, root.left) or self.find_path(head.next, root.right)
