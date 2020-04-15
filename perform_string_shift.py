439c333c010c2be914bcd7df4dad44a451ceac05
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2020-04-14 21:39:53 -0700

    [added] perform string shift

---
+++ perform_string_shift.py
@@ -0,0 +1,50 @@
+class Solution(object):
+    def stringShift(self, s, shift):
+        """
+        :type s: str
+        :type shift: List[List[int]]
+        :rtype: str
+        """
+        s = list(s)
+        for pair in shift:
+            side = pair[0]
+            number_rotation = pair[1]
+            # do left rotation
+            if side == 0:
+                s = self.left_rotation(s, number_rotation)
+
+            else:
+                s = self.right_rotation(s, number_rotation)
+            print(f"after each stage {side} and string {s}")
+        
+        return "".join(s)
+
+    def left_rotation(self, s, number_rotation):
+        """
+            rotate the string to left side by given number of times
+        """
+        if number_rotation >= len(s):
+            number_rotation = number_rotation % len(s)
+        
+        s[:] = s[number_rotation:] + s[:number_rotation]
+
+        return s
+
+    def right_rotation(self, s, number_rotation):
+        """
+            rotate the string to left side by given number of times
+        """
+        if number_rotation >= len(s):
+            number_rotation = number_rotation % len(s)
+        # s[-k:] last k items in the array
+        # s[:-k] everything but not last k items
+        s[:] = s[-number_rotation:] + s[:-number_rotation]
+
+        return s
+
+
+word = "abcdefg"
+shift = [[1, 1], [1, 1], [0, 2], [1, 3]]
+obj = Solution()
+result = obj.stringShift(word, shift)
+print(result)
